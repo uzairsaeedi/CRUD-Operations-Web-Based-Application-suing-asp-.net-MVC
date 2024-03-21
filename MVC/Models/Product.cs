9489d80d4de3383425d243875c43df1d86137fcd
uzair <uzair@UZAIR_SAEEDI>
2024-03-21 23:05:33 +0500

    Add project files.

---
+++ MVC/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Category { get; set; }
+
+    public decimal? Price { get; set; }
+}
